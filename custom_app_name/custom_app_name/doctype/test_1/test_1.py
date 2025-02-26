ae0ff5ce4d625be55a849790a7fc7f216959592a
Raghu49 <rkworld786@gmail.com>
2025-02-26 13:01:46 +0530

    Added new doctype files

---
+++ custom_app_name/custom_app_name/doctype/test_1/test_1.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, custom_app_name and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Test1(Document):
+	pass
---
+++ custom_app_name/custom_app_name/doctype/test_1/test_test_1.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, custom_app_name and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTest1(FrappeTestCase):
+	pass
